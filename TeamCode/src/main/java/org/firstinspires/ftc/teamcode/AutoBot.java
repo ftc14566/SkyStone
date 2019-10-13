3feb6945bfcd058bb2226e93b417c3e71d8ea55e
nrleist <nrleist@gmail.com>
2019-10-13 19:32:08 -0400

    idk

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBot.java
@@ -2,7 +2,13 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.*;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
+@Autonomous(name="AutoBot", group="Linear Opmode")
 public class AutoBot {
 
 	private Hardware hardware;
@@ -11,44 +17,53 @@ public class AutoBot {
 	public double defaultDriveSpeed = 0.2;
 	public double defaultSpinSpeed = 0.2;
 	public double defaultStrafeSpeed = 0.2;
+	static final double     COUNTS_PER_MOTOR_REV    = 288 ;
+	static final double     DRIVE_GEAR_REDUCTION    = 2 ;     // This is < 1.0 if geared UP
+	static final double     WHEEL_DIAMETER_INCHES   = 6.75 ;// For figuring circumference
+	static final double     WHEEL_SEPARATION = 15.25 ;
+	static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV) /
+			(WHEEL_DIAMETER_INCHES * 3.1415);
+	static final double     TURN_SPEED              = 0.3;
+	static final double     TIME_PER_INCH           = 0.5;
+
+	public void resetEncoder(){
+		hardware.frontLeftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+		hardware.frontRightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+		hardware.rearLeftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+		hardware.rearRightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+	}
+	public void encoderRun() {
+		hardware.frontLeftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+		hardware.frontRightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+		hardware.rearLeftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+		hardware.rearRightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+	}
+
 
 	public AutoBot(Hardware hardware, LinearOpMode opMode){
 		this.hardware = hardware;
+		resetEncoder();
 
-		// TODO: Add any autonomous-specific initialization here:
-		//hardware.frontLeftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-		//hardware.frontRightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-		//hardware.rearLeftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-		//hardware.rearRightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-	}
-
-	public void DriveForward(double inches){
-		DriveForward(inches,defaultDriveSpeed);
 	}
 
 	public void DriveForward(double inches, double speed){
-		// TODO: implement drive forward x-inches
-		while(opMode.opModeIsActive()){
-		}
-	}
 
-	public void SpinRight(double degrees){
-		SpinRight(degrees,defaultSpinSpeed);
+		while(opMode.opModeIsActive()){
+			resetEncoder();
+		}
 	}
 
 	public void SpinRight(double degrees, double speed){
-		// TODO: implement spin clockwise n-degrees
+
 		while(opMode.opModeIsActive()){
+			resetEncoder();
 		}
 	}
 
-	public void StrafeRight(double inches){
-		this.StrafeRight(inches,defaultStrafeSpeed);
-	}
-
 	public void StrafeRight(double inches,double speed){
-		// TODO: put code here that makes robot move to the right
+
 		while(opMode.opModeIsActive()) {
+			resetEncoder();
 		}
 	}
 
